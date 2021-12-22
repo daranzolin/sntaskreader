4df441e25edf3ec4780ba4dfebb1b9b4ffe867b0
David Ranzolin <daranzolin@gmail.com>
2021-12-22 10:05:58 -0800

    get s3 method

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,16 @@ response_table.sntask <- function(x) x$response_table
 description <- function(x) UseMethod("description")
 #' @export
 description.sntask <- function(x) x$description
+
+#' Get a value from task response table
+#'
+#' @param x An sntask
+#' @param what the key name
+#' @export
+get <- function(x, what) UseMethod("get")
+#' @export
+get.sntask <- function(x, what) {
+  rt <- response_table(x)
+  rt[rt$key == what, "value" ,drop = TRUE]
+}
+
